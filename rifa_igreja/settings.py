b258959760c6ba0f8ccbfe43c10b0def0d9dda5e
Igor <igorlesantos417@gmail.com>
2026-06-05 12:06:11 -0300

    Configura PostgreSQL Railway

--- rifa_igreja/settings.py
+++ rifa_igreja/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,10 +51,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'rifa_igreja.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
