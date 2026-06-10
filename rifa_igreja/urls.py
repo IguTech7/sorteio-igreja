47bc2115f9f934c27f6acfedfff7efdb9512e6dc
Igor <igorlesantos417@gmail.com>
2026-06-09 21:32:44 -0300

    Imagem comprovante

--- rifa_igreja/urls.py
+++ rifa_igreja/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rifa.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(
