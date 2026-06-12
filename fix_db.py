f25c1b66795e5a6acb73f0e78c15fa74936f2954
Igor <igorlesantos417@gmail.com>
2026-06-11 21:27:22 -0300

    fix db colunas

--- fix_db.py
+++ fix_db.py
@@ -1,12 +1,11 @@
-import os
 import django
-
+import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rifa_igreja.settings')
 django.setup()
 
 from django.db import connection
-
 cursor = connection.cursor()
 cursor.execute("ALTER TABLE rifa_registrocomprovante ADD COLUMN IF NOT EXISTS hash_imagem VARCHAR(64) DEFAULT '';")
-cursor.execute("ALTER TABLE rifa_registrocomprovante ADD COLUMN IF NOT EXISTS assinatura_transacao VARCHAR(200) DEFAULT '';")
+cursor.execute("ALTER TABLE rifa_registrocomprovante ADD COLUMN IF NOT EXISTS assinatura VARCHAR(200) DEFAULT '';")
+connection.commit()
 print("Colunas adicionadas com sucesso!")
